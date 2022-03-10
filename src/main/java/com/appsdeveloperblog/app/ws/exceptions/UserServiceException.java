5e1ebb611fda0024a79f0f200ffc89e5a991ecbd
Matt Plog <MPlogSWDev@outlook.com>
2022-03-10 17:13:44 -0600

    UserServiceException class created

---
+++ src/main/java/com/appsdeveloperblog/app/ws/exceptions/UserServiceException.java
@@ -0,0 +1,12 @@
+package com.appsdeveloperblog.app.ws.exceptions;
+
+import java.io.Serial;
+
+public class UserServiceException extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 1348771109171435607L;
+
+    public UserServiceException(String message) {
+        super(message);
+    }
+}
