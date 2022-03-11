34f1ea1c8ae8af414d0169c3ac3cd906d9fad187
Matt Plog <MPlogSWDev@outlook.com>
2022-03-11 14:24:44 -0600

    getUsers added to UserController and UserServiceImpl

--- src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
@@ -3,6 +3,8 @@ package com.appsdeveloperblog.app.ws.service;
 import com.appsdeveloperblog.app.ws.shared.UserDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService extends UserDetailsService {
 
     UserDto createUser(UserDto user);
@@ -10,4 +12,5 @@ public interface UserService extends UserDetailsService {
     UserDto getUserByUserId(String userId);
     UserDto updateUser(String id, UserDto userDto);
     void deleteUser(String id);
+    List<UserDto> getUsers(int page, int limit);
 }
--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -12,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 
@@ -81,4 +84,21 @@ public class UserController {
         return returnValue;
     }
 
+
+    @GetMapping(produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public List<UserRest> getUsers(@RequestParam(value="page", defaultValue="1") int page,
+                                   @RequestParam(value="limit", defaultValue="25") int limit) {
+        List<UserRest> returnValue = new ArrayList<>();
+
+        List<UserDto> users = userService.getUsers(page, limit);
+
+        for (UserDto userDto : users) {
+            UserRest userModel = new UserRest();
+            BeanUtils.copyProperties(userDto, userModel);
+            returnValue.add(userModel);
+
+        }
+
+        return returnValue;
+    }
 }
